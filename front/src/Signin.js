c8e260e6f4bb64a6d0a67fa37b10f82c7153666a
Julien Royer <eldebaran@gmail.com>
2018-12-21 18:04:20 +0100

    fadsaf

---
+++ front/src/Signin.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+export default class Signin extends Component {
+    onInput = e => (t => this.setState({ [t.name]: t.value }))(e.target);
+
+    onSubmit = e => {
+        e.preventDefault();
+        const { flash, ...fields } = this.state || {};
+        fetch("/auth/signin",
+            {
+                method: 'POST',
+                headers: new Headers({
+                    'Content-Type': 'application/json'
+                }),
+                body: JSON.stringify(fields),
+            })
+            .then(res => res.json())
+            .then(
+                res => this.setState({ flash: res.flash }),
+                err => this.setState({ flash: err.flash })
+            );
+    };
+
+    render() {
+        const { flash, ...fields } = this.state || {};
+        return (
+            <form onInput={this.onInput} onSubmit={this.onSubmit}>
+                <h1>Signup: {JSON.stringify(fields, undefined, 1)}</h1>
+
+                <p>Flash: {flash || ""}</p>
+
+                <p><label>Email<br /><input name="email" type="email" autoComplete="username" /></label></p>
+                <p><label>Password<br /><input name="password" type="password" autoComplete="current-password" /></label></p>
+                <p><input type="submit" value="Submit" /></p>
+            </form>
+        );
+    }
+}
