4319b287713fe0ac419ec566ff01687160191198
Julien Royer <eldebaran@gmail.com>
2018-12-21 10:39:28 +0100

    dsfdfsa

--- front/src/Signup.js
+++ front/src/Signup.js
@@ -8,10 +8,17 @@ export default class Signup extends Component {
 
     render() {
         return (
-            <div>
-                <h1>{this.state.email}</h1>
-                <input type="email" name="email" defaultValue={this.props.email} onInput={e => this.setState({ email: e.target.value })} />
-            </div>
+            <form>
+                <h1>Inscription pour {this.state.email}</h1>
+
+                <p><input name="email" type="email" defaultValue={this.props.email}
+                    onInput={e => this.setState({ email: e.target.value })} /></p>
+                <p><input name="password" type="password" /></p>
+                <p><input name="passwordbis" type="password" /></p>
+                <p><input name="name" /></p>
+                <p><input name="lastname" /></p>
+                <p><input type="submit" value="Soumettre" /></p>
+            </form>
         );
     }
 }
