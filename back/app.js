d63d54d3bb687ae854dc9607f4d872a77c750cdc
Julien Royer <eldebaran@gmail.com>
2018-12-21 14:24:57 +0100

    dsafsaf

---
+++ back/app.js
@@ -0,0 +1,24 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (_req, res) => {
+    res.send("youhou");
+});
+
+app.use((_req, _res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Listening on port ' + server.address().port);
+});
