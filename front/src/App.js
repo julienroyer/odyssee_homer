a4553b2067acaefd22d2fc52a81656d072c8184f
Julien Royer <eldebaran@gmail.com>
2018-12-21 17:59:43 +0100

    sdfaaasfdsa

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Signup from './Signup';
 
-const theme = createMuiTheme({
-  typography: {
-    useNextVariants: true,
-  },
-});
-export default () => <MuiThemeProvider theme={theme}><Signup /></MuiThemeProvider>;
+export default () => <Signup />;
