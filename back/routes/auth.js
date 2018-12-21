f85db26eefec5e425999f28c58fe106169eec10c
Julien Royer <eldebaran@gmail.com>
2018-12-21 15:42:58 +0100

    fdsdafds

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,3 +1,4 @@
+const connection = require('../helpers/db');
 const express = require('express');
 
 const router = express.Router();
@@ -5,7 +6,7 @@ const router = express.Router();
 router.post('/signup', (req, res) => {
     connection.query('INSERT INTO users SET ?', req.body, error => {
         if (error) {
-            res.status(500).send('Error: ' + error).end();
+            res.status(500).send(error).end();
         } else {
             res.end();
         }
