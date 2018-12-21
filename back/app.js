1a0a7f7be375bcccf45beb4efeb85295d0fc8d93
Julien Royer <eldebaran@gmail.com>
2018-12-21 15:50:22 +0100

    fsadf

--- back/app.js
+++ back/app.js
@@ -22,6 +22,6 @@ app.use((_req, _res, next) => {
     next(err);
 });
 
-const server = app.listen(process.env.PORT || 3000, () => {
-    console.log('Listening on port ' + server.address().port);
+const server = app.listen(process.env.PORT || 5000, () => {
+    console.log(`Listening on port ${server.address().port}`);
 });
