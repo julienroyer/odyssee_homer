26aa42d411864f10ba946fa42a9f9d0d0d39984f
Julien Royer <eldebaran@gmail.com>
2018-12-21 17:12:34 +0100

    faasddsa

--- front/src/App.js
+++ front/src/App.js
@@ -1,30 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Signup from './Signup';
-import logo from './logo.svg';
-import './App.css';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <main>
-          <Signup />
-        </main>
-      </div>
-    );
-  }
-}
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+});
+export default () => <MuiThemeProvider theme={theme}><Signup /></MuiThemeProvider>;
