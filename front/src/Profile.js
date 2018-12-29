cb3ae8469bbbd4e4c90370cc0758c6c3cd269b14
Julien Royer <eldebaran@gmail.com>
2018-12-29 19:01:39 +0100

    fadfsafd

--- front/src/Profile.js
+++ front/src/Profile.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-export default class Profile extends Component {
+class Profile extends Component {
     constructor(props) {
         super(props);
         this.state = {};
     }
 
     render() {
-        return (
-            <dl>
+        return [
+            <button key="0" onClick={() => this.props.history.push('/signin')}>Log out</button>,
+            <dl key="1">
                 <dt>Email</dt>
                 <dd>{this.state.email}</dd>
                 <dt>Name</dt>
                 <dd>{this.state.name}</dd>
                 <dt>Last name</dt>
                 <dd>{this.state.lastname}</dd>
-            </dl>
-        );
+            </dl>,
+        ];
     }
 }
+
+export default withRouter(Profile);
