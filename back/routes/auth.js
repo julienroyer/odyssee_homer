fd5724cd4fa000a60baf761a1f564c1fb2baaeae
Julien Royer <eldebaran@gmail.com>
2019-01-09 16:53:19 +0100

    afdsf

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const connection = require('../helpers/db');
 
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     const values = ['email', 'password', 'name', 'lastname'].reduce((a, v) => {
         const val = req.body[v];
         a[v] = (val && String(val).trim()) || undefined;
         return a;
     }, {});
+    values.password = await bcrypt.hash(values.password, 10);
     connection.query('INSERT INTO users SET ?', values, error => {
         if (error) {
             res.status(500).json({ flash: error.message }).end();
