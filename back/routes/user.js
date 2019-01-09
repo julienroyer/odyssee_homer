89d57554dd7d018c4782bb10f62aa2604c6ab4ea
Julien Royer <eldebaran@gmail.com>
2019-01-09 16:11:23 +0100

    fafd

--- back/routes/user.js
+++ back/routes/user.js
@@ -5,14 +5,13 @@ const router = express.Router();
 
 router.get('/:email/profile', (req, res) => {
     const email = req.params.email;
-    connection.query('SELECT * FROM users WHERE email=?', [email], (error, result) => {
+    connection.query('SELECT name, lastname FROM users WHERE email=?', [email], (error, result) => {
         if (error) {
             res.status(500).json({ flash: error.message }).end();
         } else if (!result.length) {
             res.status(404).json({ flash: `User '${email}' not found` }).end();
         } else {
-            const { id, password, ...profile } = result[0];
-            res.json(profile).end();
+            res.json(result[0]).end();
         }
     });
 });
