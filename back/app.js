cf5b9744791ff6adc4fd5e1e07b881cb8f546f33
Julien Royer <eldebaran@gmail.com>
2019-01-10 17:41:48 +0100

    fsdasa

--- back/app.js
+++ back/app.js
@@ -1,8 +1,11 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const authRouter = require('./routes/auth');
 const userRouter = require('./routes/user');
+const connection = require('./helpers/db');
 
 const app = express();
 
@@ -23,6 +26,28 @@ app.use((_req, _res, next) => {
     err.status = 404;
     next(err);
 });
+
+passport.use(new LocalStrategy(
+    {
+        usernameField: 'email',
+        passwordField: 'password',
+        session: false,
+    },
+    (email, password, done) =>
+        connection.query('SELECT password FROM users WHERE email=?', [email], async (error, result) => {
+            if (error) {
+                done(err);
+            } else {
+                const entry = result[0];
+                const match = await bcrypt.compare(password, entry ? entry.password : '$2b$10$TRUiCb7DnUDKN0544viAZ.cZNey36JuR3vxm7MjECG7yY9NR6HVeS');
+                if (entry && match) {
+                    done(null, { email, }, { flash: 'User has been signed in!', });
+                } else {
+                    done(null, false, { flash: 'Invalid credentials', });
+                }
+            }
+        })
+));
 
 const server = app.listen(process.env.PORT || 5000, () => {
     console.log(`Listening on port ${server.address().port}`);
