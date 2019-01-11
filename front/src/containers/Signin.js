89aecd4a035e5ba5c7e5f098f93c38a2b5276249
Julien Royer <eldebaran@gmail.com>
2019-01-11 16:57:51 +0100

    fad

--- front/src/containers/Signin.js
+++ front/src/containers/Signin.js
@@ -18,7 +18,7 @@ export default class Signin extends React.Component {
                 body: JSON.stringify(fields),
             }
         ).then(res => res.json().then(
-            obj => res.ok ? this.props.history.push(`/profile/${fields.email}`) : this.setState({ flash: obj.flash }),
+            obj => res.ok ? this.props.history.push(`/profile/${fields.email}`) : this.setState({ flash: obj.flash || 'Unknown error' }),
             () => this.setState({ flash: `Request failure (HTTP ${res.status})` }))
         ).catch(err => this.setState({ flash: `Request failure (${err.message})` }));
     };
