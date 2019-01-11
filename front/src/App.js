c3d1a764f759ba73cf854b44ab6e1ba993b9f8d6
Julien Royer <eldebaran@gmail.com>
2019-01-11 17:33:04 +0100

    fddfsa

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect, Link } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import Signin from './containers/Signin';
 import Signup from './containers/Signup';
 import Profile from './containers/Profile';
+import reducers from './reducers';
+
+const store = createStore(reducers);
 
 export default () => (
     <React.StrictMode>
-        <Router>
-            <Switch>
-                <Redirect exact from="/" to="/signin" />
-                <Route path="/signin" component={Signin} />
-                <Route path="/signup" component={Signup} />
-                <Route path="/profile/:email" component={Profile} />
-                <Route render={({ location }) => <>
-                    <p>The requested URL <code>{location.pathname}</code> was not found.</p>
-                    <p><Link to="/">Home</Link></p>
-                </>} />
-            </Switch>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Redirect exact from="/" to="/signin" />
+                    <Route path="/signin" component={Signin} />
+                    <Route path="/signup" component={Signup} />
+                    <Route path="/profile/:email" component={Profile} />
+                    <Route render={({ location }) => <>
+                        <p>The requested URL <code>{location.pathname}</code> was not found.</p>
+                        <p><Link to="/">Home</Link></p>
+                    </>} />
+                </Switch>
+            </Router>
+        </Provider>
     </React.StrictMode>
 );
