5e6983ba0c1f28a9f990b21d5b13b54d4501534b
Julien Royer <eldebaran@gmail.com>
2019-01-11 16:51:41 +0100

    fadsaf

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect, Link } from 'react-router-dom';
 import Signin from './containers/Signin';
 import Signup from './containers/Signup';
 import Profile from './containers/Profile';
