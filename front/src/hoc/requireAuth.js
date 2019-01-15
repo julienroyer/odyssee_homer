6843de0d74019e8f563452ccbd31b5f8b954b293
Julien Royer <eldebaran@gmail.com>
2019-01-15 15:52:24 +0100

    afdsaf

--- front/src/hoc/requireAuth.js
+++ front/src/hoc/requireAuth.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-export default Component => connect(({ auth }) => ({ authenticated: Boolean(auth.user.token) }))(
+export default Component => connect(({ auth }) => ({ authenticated: Boolean(auth.user) }))(
     ({ authenticated, ...rest }) => authenticated ? <Component {...rest} /> : <Redirect to="/" />
 );
