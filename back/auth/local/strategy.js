9c3df5b3ed9a27625c19f64e44c293f02bd684a0
Julien Royer <eldebaran@gmail.com>
2019-01-18 17:55:49 +0100

    f

--- back/auth/local/strategy.js
+++ back/auth/local/strategy.js
@@ -10,5 +10,5 @@ module.exports = new LocalStrategy({ usernameField: 'email' }, async (email, pas
     const [[entry]] = await asyncDbQuery('SELECT password FROM users WHERE email=?', email);
     const match = await bcrypt.compare(password,
         entry ? entry.password : '$2b$10$TRUiCb7DnUDKN0544viAZ.cZNey36JuR3vxm7MjECG7yY9NR6HVeS');
-    done(null, entry && match ? { email } : null);
+    done(null, entry && match && { email });
 });
