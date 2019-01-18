2f439104cbd8bd265949ae4f389bc8b554a17864
Julien Royer <eldebaran@gmail.com>
2019-01-18 17:27:49 +0100

    fddfsa

--- back/auth/router.js
+++ back/auth/router.js
@@ -9,8 +9,8 @@ const localAuth = require('./local/authenticator');
 
 const router = express.Router();
 
-const asyncDbQuery = asyncFn(dbPool.query.bind(dbPool));
-const asyncJwtSign = asyncFn(jwt.sign.bind(jwt));
+const asyncJwtSign = asyncFn(jwt.sign);
+const asyncDbQuery = asyncFn((...args) => dbPool.query(...args));
 
 router.post('/signup', asyncMw(async (req, res) => {
     const values = ['email', 'password', 'name', 'lastname'].reduce((a, v) => {
