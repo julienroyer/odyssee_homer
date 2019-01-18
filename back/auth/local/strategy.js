bc2c9bb991d68210707fb0f2b51ec4e41f4e9021
Julien Royer <eldebaran@gmail.com>
2019-01-18 17:52:09 +0100

    fsad

--- back/auth/local/strategy.js
+++ back/auth/local/strategy.js
@@ -8,8 +8,7 @@ const asyncDbQuery = asyncFn((...args) => dbPool.query(...args));
 // TODO properly handle async
 module.exports = new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
     const [[entry]] = await asyncDbQuery('SELECT password FROM users WHERE email=?', email);
-    console.log(email)
     const match = await bcrypt.compare(password,
         entry ? entry.password : '$2b$10$TRUiCb7DnUDKN0544viAZ.cZNey36JuR3vxm7MjECG7yY9NR6HVeS');
-    done(null, entry && match ? { email } : null, 'a');
+    done(null, entry && match ? { email } : null);
 });
