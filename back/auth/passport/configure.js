eefb8fb5f0eec7ee94fd1da99093ff9cfb481c16
Julien Royer <eldebaran@gmail.com>
2019-01-22 10:13:24 +0100

    m

--- back/auth/passport/configure.js
+++ back/auth/passport/configure.js
@@ -1,6 +1,5 @@
-const passport = require('passport');
-
-module.exports = () => {
+module.exports = exports = () => {
+    const passport = require('passport');
     passport.use(require('./local/strategy'));
     passport.use(require('./jwt/strategy'));
 };
