10a9b2eadfa883d2d006f249d805e1ec31e05bf1
Julien Royer <eldebaran@gmail.com>
2019-01-24 11:33:58 +0100

    m

--- back/auth/passport/local/strategy.js
+++ back/auth/passport/local/strategy.js
@@ -6,7 +6,9 @@ const dbPool = require('../../../helpers/db/pool');
 const { asyncMw } = require('../../../helpers/async-wrappers');
 
 module.exports = exports =
-    new LocalStrategy({ usernameField: 'email' }, asyncMw(async (email, password, done) => {
+    new LocalStrategy({ usernameField: 'email', session: false }, asyncMw(async (email, password, done) => {
+        //email = email.trim();
+        //password = password.trim();
         const [entry] = await dbPool.awaitableQuery('SELECT password FROM users WHERE email=?', email);
         const match = await bcrypt.compare(password,
             entry ? entry.password : '$2b$10$TRUiCb7DnUDKN0544viAZ.cZNey36JuR3vxm7MjECG7yY9NR6HVeS');
